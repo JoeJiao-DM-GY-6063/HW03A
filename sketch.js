56a1d97b92c9c030578edbb769c6c80dcb37b333
LazyJoe666 <Joeao1026@gmail.com>
2024-09-27 21:32:57 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,24 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 800);
+  background(235, 210, 47)
+  for (let d = 30; d < 600; d+=75) {
+    for (let c = 30; c < 900; c+=75) {
+      fill(13, 13, 13);
+      ellipse(d, c, 30, 30); 
+      
+    }
+    
+    
+  }
+  for (let d = 67.5; d < 570; d+=75) {
+    for (let c = 67.5; c < 900; c+=75) {
+      fill(13, 13, 13);
+      ellipse(d, c, 10, 10); 
+      
+    }
+    
+    
+  }
 }
 
 function draw() {
